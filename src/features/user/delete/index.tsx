4e16ff659072d54c237dc222bd720d3c1bc425a0
anko011 <ant.kov.vl@yandex.ru>
2023-08-02 15:01:01 +0900

    fix: added reload page when deleted user

--- src/features/user/delete/index.tsx
+++ src/features/user/delete/index.tsx
@@ -4,41 +4,34 @@ import {fetchDeleteUser} from "@/entities/user";
 import {useNotification} from "@/share/client/hooks";
 import {getDeleteUserFeautureDictionary} from "@/features/user/delete/i18n";
 import {modals} from "@mantine/modals";
+import {useRouter} from "next/router";
 
 interface DeleteUserButtonProps {
     userId: number
-
-    onDeleteUser?(): void
 }
 
 const dictionary = getDeleteUserFeautureDictionary('ru')
 
 
-export const DeleteUserButton = ({userId, onDeleteUser}: DeleteUserButtonProps) => {
+export const DeleteUserButton = ({userId}: DeleteUserButtonProps) => {
     const notification = useNotification(dictionary.notification.title)
-
+    const router = useRouter()
     const handleClick = async () => {
         modals.openConfirmModal({
             title: dictionary.modal.title,
+            centered: true,
             children: <Text>Вы действительно хотите удалить пользователя?</Text>,
             labels: {
                 cancel: dictionary.modal.buttons.cancel,
                 confirm: dictionary.modal.buttons.confirm
             },
-            confirmProps: {
-                color: 'red'
-            },
-            centered: true,
+            confirmProps: {color: 'red'},
             onConfirm: async () => {
                 const response = await fetchDeleteUser(userId)
                 const data = await response.json()
 
-                if (!response.ok) {
-                    notification.errorNotify(data?.message ?? dictionary.notification.error)
-                } else {
-                    notification.successNotify(dictionary.notification.success)
-                    onDeleteUser?.()
-                }
+                notification.byResponseNotify(response, dictionary.notification.success, data.message ?? dictionary.notification.error)
+                router.replace(router.asPath)
             }
         })
     }
